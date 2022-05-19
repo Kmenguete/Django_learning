0e9bbefb00ebcd9266a0929f430b4b55d41113e9
Kmenguete <menguetekevin@gmail.com>
2022-05-19 17:09:49 +0200

    displaying correctly title data in Django admin user interface

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 
 from listings.models import Band
 
+from listings.models import Title
+
 
 class BandAdmin(admin.ModelAdmin):
     list_display = ('name', 'year_formed', 'genre')  # fields we want to display in the band list of Django admin user
     # interface
 
 
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('title', )  # fields we want to display in the title list of Django admin user interface
+
+
 admin.site.register(Band, BandAdmin)
+
+admin.site.register(Title, TitleAdmin)
