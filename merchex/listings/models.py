73388fc1407d0c2a08260397afac91d4db7c4782
Kmenguete <menguetekevin@gmail.com>
2022-05-19 13:54:22 +0200

    adding rules for each field

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
-    genre = models.fields.CharField()
-    biography = models.fields.CharField()
-    year_formed = models.fields.IntegerField()
-    active = models.fields.BooleanField()
-    official_homepage = models.fields.URLField()
+    genre = models.fields.CharField(max_length=50)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2021)])
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
 
 
 class Title(models.Model):
