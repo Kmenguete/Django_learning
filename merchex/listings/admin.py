379dd5df3434e0b631e412a142b0c3d87461c1a9
Kmenguete <menguetekevin@gmail.com>
2022-05-19 16:57:40 +0200

    adding data to display in Django admin user interface for bands

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from listings.models import Band
 
-admin.site.register(Band)
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year_formed', 'genre')  # fields we want to display in the band list of Django admin user
+    # interface
+
+
+admin.site.register(Band, BandAdmin)
