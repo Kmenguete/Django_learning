61a7e2dd9bcd6b9fc15c3a2e3bbd055d2e6068a0
Kmenguete <menguetekevin@gmail.com>
2022-05-19 15:06:24 +0200

    defining which genres are allowed for Band object

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -3,8 +3,15 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Band(models.Model):
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
     name = models.fields.CharField(max_length=100)
-    genre = models.fields.CharField(max_length=50)
+    genre = models.fields.CharField(max_length=5)
+    choices = Genre.choices
     biography = models.fields.CharField(max_length=1000)
     year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2021)])
     active = models.fields.BooleanField(default=True)
