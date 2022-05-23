3e374b4ad75c34c7656cd0db50adde0f1cf78077
Kmenguete <menguetekevin@gmail.com>
2022-05-23 17:22:34 +0200

    updating url path to band creation

--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('bands/', views.band_list, name='band-list'),
     path('bands/<int:id>/', views.band_detail, name='band-detail'),
     path('bands/add/', views.band_create, name='band-create'),
-    path('about-us/', views.about),
+    path('about-us/', views.about, name='about'),
     path('listing/', views.listings, name='listings'),
     path('listing/<int:id>/', views.listing_detail, name='listing-detail'),
     path('contact-us/', views.contact, name='contact'),
