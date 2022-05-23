cf8077b8a5cfd2a49a18e7bbe33cf115535f4300
Kmenguete <menguetekevin@gmail.com>
2022-05-23 17:21:36 +0200

    updating url path to band creation

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from listings.models import Band
 from listings.models import Title
-from listings.forms import ContactUsForm
+from listings.forms import BandForm, ContactUsForm
 from django.core.mail import send_mail
 from django.shortcuts import redirect
 
@@ -52,4 +52,5 @@ def email_sent(request):
 
 
 def band_create(request):
-    return render(request, 'listings/band_create.html')
+    form = BandForm()
+    return render(request, 'listings/band_create.html', {'form': form})
